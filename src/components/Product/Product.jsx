c919441d957afb96f98d2e4358bce3ff6e5ab2ca
MH-Kniloy <web.kniloy@gmail.com>
2025-03-29 04:48:21 +0600

    Product info added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 
 const Product = ({product}) => {
     console.log(product);
-    const {brand, battery, display, image, model, price, processor, ram, release_year, storage} = product
+    const {brand, battery, image, model, price, processor, ram, release_year, storage} = product
   return (
     <>
      <div className='card'>
-        {brand}
-        <h4>{price}</h4>
+        <h2>{brand}</h2>
+        <h3>{model}</h3>
+        <p>Processor {processor}</p>
+        <p>Ram {ram}</p>
+        <p>{storage}</p>
+        <p>battery {battery} mah</p>
+        <p>{release_year}</p>
+        <h4>Price : {price}</h4>
         <img width={400} height={300} src={image} alt="" />
+        <button>Add to cart</button>
      </div>
     </>
   )
